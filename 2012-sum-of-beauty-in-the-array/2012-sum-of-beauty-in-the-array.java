0fb44456a7181c5d8963d73e5b27a110f4271709
Dhanush Rajulapati <dhanushrajulapati2005@gmail.com>
2026-07-25 22:53:58 +0530

    Time: 6 ms (95.59%), Space: 98.5 MB (6.37%) - LeetHub

--- 2012-sum-of-beauty-in-the-array/2012-sum-of-beauty-in-the-array.java
+++ 2012-sum-of-beauty-in-the-array/2012-sum-of-beauty-in-the-array.java
@@ -2,23 +2,21 @@ class Solution {
     public int sumOfBeauties(int[] nums) {
         int n = nums.length;
         int []suf = new int[n];
-
         suf[n-1] = nums[n-1];
-
-        for(int i=1;i<n;i++) {
-            suf[n-i-1] = Math.min(suf[n-i],nums[n-i]);
+        for(int i=n-2;i>=0;i--) {
+            suf[i] = Math.min(suf[i+1],nums[i+1]);
         }
 
+        int left = nums[0];
         int res = 0;
-        int pre = nums[0];
         for(int i=1;i<=n-2;i++) {
-            if(nums[i] > pre && nums[i] < suf[i]) {
-                res += 2;
+            if(left < nums[i] && nums[i] <suf[i]) {
+                res+=2;
             }
-            else if(nums[i] > nums[i-1] && nums[i] < nums[i+1]) {
+            else if(nums[i-1] < nums[i] && nums[i] < nums[i+1]) {
                 res++;
             }
-            pre = Math.max(pre,nums[i]);
+            left = Math.max(left,nums[i]);
         }
         return res;
     }
