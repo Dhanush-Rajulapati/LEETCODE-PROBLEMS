d99659b70974b43fd79684755b41c729eb9b2191
Dhanush Rajulapati <dhanushrajulapati2005@gmail.com>
2026-08-08 19:00:28 +0530

    Time: 171 ms (74.63%), Space: 80.7 MB (29.2%) - LeetHub

--- 1711-count-good-meals/1711-count-good-meals.java
+++ 1711-count-good-meals/1711-count-good-meals.java
@@ -1,18 +1,16 @@
 class Solution {
     int MOD = 1000000007;
     public int countPairs(int[] deliciousness) {
-        int []power = new int[22];
-        for(int i=0;i<=21;i++) {
-            power[i] = (1<<i);
-        }
         long res = 0;
         HashMap<Integer,Integer> set = new HashMap<>();
         for(int num : deliciousness) {
-            for(int p : power) {
+            int p = 1;
+            for(int i=0;i<=21;i++) {
                 res += set.getOrDefault(p-num,0);
                 if(res > MOD) {
                     res = res%MOD;
                 }
+                p <<= 1;
             }
             set.put(num,set.getOrDefault(num,0)+1);
         }
