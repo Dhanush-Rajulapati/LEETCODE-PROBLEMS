3ba705543e2610dc633915fe9091b6450f8785a3
Dhanush Rajulapati <dhanushrajulapati2005@gmail.com>
2026-08-24 21:08:40 +0530

    Time: 53 ms (76.63%), Space: 43.7 MB (58.7%) - LeetHub

--- 2947-count-beautiful-substrings-i/2947-count-beautiful-substrings-i.java
+++ 2947-count-beautiful-substrings-i/2947-count-beautiful-substrings-i.java
@@ -1,13 +1,13 @@
 class Solution {
     public int beautifulSubstrings(String s, int k) {
-        String vowels = "aeiou";
         int res = 0;
         int n = s.length();
         for(int i=0;i<n;i++) {
             int v = 0;
             int c = 0;
             for(int j=i;j<n;j++) {
-                if(vowels.indexOf(s.charAt(j)) != -1) {
+                char ch = s.charAt(j);
+                if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
                     v++;
                 }
                 else {
