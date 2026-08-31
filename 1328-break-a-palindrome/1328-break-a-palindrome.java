e10f7d3f9aee03beb6f5b63be7b28ea4064741ba
Dhanush Rajulapati <dhanushrajulapati2005@gmail.com>
2026-08-31 20:56:34 +0530

    Time: 0 ms (100%), Space: 42.7 MB (89.23%) - LeetHub

---
+++ 1328-break-a-palindrome/1328-break-a-palindrome.java
@@ -0,0 +1,17 @@
+class Solution {
+    public String breakPalindrome(String palindrome) {
+        int n = palindrome.length();
+        if(n == 1) {
+            return "";
+        }
+        char []res = palindrome.toCharArray();
+        for(int i=0;i<n/2;i++) {
+            if(res[i] != 'a') {
+                res[i] = 'a';
+                return new String(res);
+            }
+        }
+        res[n-1] = 'b';
+        return new String(res);
+    }
+}
