a31be561659669b8fc19f55c999374a3e5454540
Dhanush Rajulapati <dhanushrajulapati2005@gmail.com>
2026-09-09 05:33:30 +0530

    Time: 1 ms (99.36%), Space: 42.7 MB (37.18%) - LeetHub

--- 3871-count-commas-in-range-ii/3871-count-commas-in-range-ii.java
+++ 3871-count-commas-in-range-ii/3871-count-commas-in-range-ii.java
@@ -1,14 +1,5 @@
 class Solution {
     public long countCommas(long n) {
-        // if(n < 1000)
-        //     return 0;
-        // if(n < 1000000)
-        //     return n-999;
-        // if(n < 1000000000)
-        //     return 999000+(n-999999)*2;
-        // if(n < 1000000000000L)
-        //     return 1998999000L+(n-999999999L)*3;
-        // return 2998998999000L+(n-999999999999L)*4;
         long ans = 0;
         long base = 1000;
         long c = 1;
